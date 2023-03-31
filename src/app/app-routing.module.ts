aaa10fb7febd41ae28a1e8bed48d7822cae04dd0
Christian Oller <christianoller.dev@gmail.com>
2023-03-31 16:00:39 +0200

    Create the number of corresponding pages per section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { FeaturesPageComponent } from "./components/features-page/features-page.component";
+import { CareerPageComponent } from "./components/career-page/career-page.component";
+import { VideoPageComponent } from "./components/video-page/video-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "features-page",
+    component: FeaturesPageComponent,
+  },
+  {
+    path: "carrer-page",
+    component: CareerPageComponent,
+  },
+  {
+    path: "video-page",
+    component: VideoPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
